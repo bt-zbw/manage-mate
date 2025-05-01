9d3dbe15dbace81ff0968d85f6fb6dc6f0a6cfcf
giordano-ch <benjamin.taccone@giordano.ch>
2025-05-01 15:28:46 +0200

    [ADD] manage mate project

---
+++ Application/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Application.Models;
+
+namespace Application.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext (DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Application.Models.Contact> Contact { get; set; } = default!;
+    }
+}
